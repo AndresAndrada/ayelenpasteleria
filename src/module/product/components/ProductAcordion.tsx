5c16463244fc4bc8508326882f27db4ee83a55e9
unknown <andresandrada1994@gmail.com>
2025-09-26 18:08:38 -0300

    change cartcard

--- src/module/product/components/ProductAcordion.tsx
+++ src/module/product/components/ProductAcordion.tsx
@@ -14,7 +14,7 @@ export const ProductAcordion = () => {
         <input type="radio" name="my-accordion-4" className="" />
         <div className="m-0 p-0 collapse-title font-semibold bg-banner bg-cover">
           <div className="w-full h-full flex justify-center items-center bg-black/45">
-            <h3 className="text-secondary">Torta</h3>
+            <h3 className="text-teal-500">Torta</h3>
           </div>
         </div>
         <div className="collapse-content text-sm flex flex-col gap-2">
@@ -36,7 +36,7 @@ export const ProductAcordion = () => {
         <input type="radio" name="my-accordion-4" />
         <div className="m-0 p-0 collapse-title font-semibold text-white bg-bannerGalletas bg-cover bg-[center_70px]">
           <div className="w-full h-full flex justify-center items-center bg-black/45">
-            <h3 className="text-secondary">Galletas</h3>
+            <h3 className="text-teal-500">Galletas</h3>
           </div>
         </div>
         <div className="collapse-content text-sm flex flex-col gap-2">
@@ -58,7 +58,7 @@ export const ProductAcordion = () => {
         <input type="radio" name="my-accordion-4" />
         <div className="m-0 p-0 collapse-title font-semibold text-white bg-bannerMerienda bg-cover bg-[center_100px]">
           <div className="w-full h-full flex justify-center items-center bg-black/45">
-            <h3 className="text-secondary">Meriendas</h3>
+            <h3 className="text-teal-500">Meriendas</h3>
           </div>
         </div>
         <div className="collapse-content text-sm flex flex-col gap-2">
@@ -80,7 +80,7 @@ export const ProductAcordion = () => {
         <input type="radio" name="my-accordion-4" />
         <div className="m-0 p-0 collapse-title font-semibold text-white bg-bannerAlfajores bg-cover bg-[center_120px]">
           <div className="w-full h-full flex justify-center items-center bg-black/45">
-            <h3 className="text-secondary">Alfajores</h3>
+            <h3 className="text-teal-500">Alfajores</h3>
           </div>
         </div>
         <div className="collapse-content text-sm flex flex-col gap-2">
