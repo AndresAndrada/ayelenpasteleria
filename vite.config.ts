176156a30a951dd90dc5f74c697a5b2b3180477e
unknown <andresandrada1994@gmail.com>
2025-09-26 17:01:51 -0300

    Add vercel file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true, 
+    rollupOptions: {
+      input: {
+        main: './index.html', // Asegura que index.html sea el entry
+      },
+    },
+  },
 })
